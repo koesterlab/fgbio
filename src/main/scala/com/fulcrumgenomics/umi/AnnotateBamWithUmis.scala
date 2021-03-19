b141c3b102743426939d0cb471ff8423b481a1ea
Nils Homer <nilshomer@gmail.com>
2021-03-19 09:31:41 -0700

    Add multi-FASTQ input support to AnnotateBamWithUmis

--- src/main/scala/com/fulcrumgenomics/umi/AnnotateBamWithUmis.scala
+++ src/main/scala/com/fulcrumgenomics/umi/AnnotateBamWithUmis.scala
@@ -36,12 +36,14 @@ import com.fulcrumgenomics.util.{ProgressLogger, ReadStructure, SegmentType}
 @clp(description =
   """
     |Annotates existing BAM files with UMIs (Unique Molecular Indices, aka Molecular IDs,
-    |Molecular barcodes) from a separate FASTQ file. Takes an existing BAM file and a FASTQ
-    |file consisting of UMI reads, matches the reads between the files based on read names,
+    |Molecular barcodes) from  separate FASTQ files. Takes an existing BAM file and one or more FASTQ
+    |files consisting of UMI reads, matches the reads between the files based on read names,
     |and produces an output BAM file where each record is annotated with an optional tag
     |(specified by `attribute`) that contains the read sequence of the UMI.  Trailing read
     |numbers (`/1` or `/2`) are removed from FASTQ read names, as is any text after whitespace,
-    |before matching.
+    |before matching. If multiple UMI segments are specified (see `--read-structure`) across one
+    |or more FASTQs, they are delimited in the same order as FASTQs are specified on the command
+    |line.  The delimiter is controlled by the `--delimiter` option.
     |
     |The `--read-structure` option may be used to specify which bases in the FASTQ contain UMI
     |bases.  Otherwise it is assumed the FASTQ contains only UMI bases.
@@ -52,20 +54,31 @@ import com.fulcrumgenomics.util.{ProgressLogger, ReadStructure, SegmentType}
     |may be specified to cause the program to terminate the first time it finds a records
     |without a matching UMI.
     |
-    |In order to avoid sorting the input files, the entire UMI fastq file is read into
+    |In order to avoid sorting the input files, the entire UMI fastq file(s) is read into
     |memory. As a result the program needs to be run with memory proportional the size of
-    |the (uncompressed) fastq.
+    |the (uncompressed) fastq(s).
   """,
   group = ClpGroups.SamOrBam)
 class AnnotateBamWithUmis(
   @arg(flag='i', doc="The input SAM or BAM file.")             val input: PathToBam,
-  @arg(flag='f', doc="Input FASTQ file with UMI reads.")       val fastq: PathToFastq,
+  @arg(flag='f', doc="Input FASTQ(s) with UMI reads.")         val fastq: Seq[PathToFastq],
   @arg(flag='o', doc="Output BAM file to write.")              val output: PathToBam,
   @arg(flag='t', doc="The BAM attribute to store UMIs in.")    val attribute: String = "RX",
   @arg(flag='r', doc="The read structure for the FASTQ, otherwise all bases will be used.")
-                                                               val readStructure: ReadStructure = ReadStructure("+M"),
+                                                               val readStructure: Seq[ReadStructure] = Seq(ReadStructure("+M")),
+  @arg(flag='d', doc="The UMI delimiter")                      val delimiter: String = "-",
   @arg(          doc="If set, fail on the first missing UMI.") val failFast: Boolean = false
 ) extends FgBioTool with LazyLogging {
+
+  Io.assertReadable(Seq(input) ++ fastq)
+  Io.assertCanWriteFile(output)
+  validate(readStructure.length == 1 || readStructure.length == fastq.length,
+    "The number of read structures must match the number of FASTQs, or only a single read structure must be given"
+  )
+  private val structures: Seq[ReadStructure] = {
+    if (readStructure.length == fastq.length) readStructure
+    else Seq.tabulate(3)(_ => readStructure.head)
+  }
 
   private var missingUmis: Long = 0
 
@@ -76,23 +89,25 @@ class AnnotateBamWithUmis(
   }
 
   /** Extracts the UMI bases given the read structure */
-  private def extractUmis(bases: String, structure: ReadStructure): String = {
+  private def extractUmis(bases: String, structure: ReadStructure): Seq[String] = {
     structure
       .extract(bases)
       .filter(_.kind == SegmentType.MolecularBarcode)
       .map(_.bases)
-      .mkString("")
   }
 
   /** Main method that does the work of reading input files, matching up reads and writing the output file. */
   override def execute(): Unit = {
-    Io.assertReadable(Seq(input, fastq))
-    Io.assertCanWriteFile(output)
-
     // Read in the fastq file
     logger.info("Reading in UMIs from FASTQ.")
-    val fqIn      = FastqSource(fastq)
-    val nameToUmi =  fqIn.map(fq => (fq.name, extractUmis(fq.bases, readStructure))).toMap
+    val fqSources = fastq.map(FastqSource.apply)
+    val nameToUmi = FastqSource.zipped(sources=fqSources).map { records =>
+      val umi = records.zip(structures).flatMap { case (fq, structure) =>
+        extractUmis(bases=fq.bases, structure=structure)
+      }.mkString(delimiter)
+      (records.head.name, umi)
+    }.toMap
+    fqSources.foreach(_.close())
 
     // Loop through the BAM file an annotate it
     logger.info("Reading input BAM and annotating output BAM.")
--- src/test/scala/com/fulcrumgenomics/umi/AnnotateBamWithUmisTest.scala
+++ src/test/scala/com/fulcrumgenomics/umi/AnnotateBamWithUmisTest.scala
@@ -39,29 +39,51 @@ class AnnotateBamWithUmisTest extends UnitSpec {
   private val fq  = dir.resolve("annotate_umis.fastq")
   private val umiTag    = "RX"
 
-  "AnnotateBamWithUmis" should "successfully add UMIs to a BAM in" in {
+  "AnnotateBamWithUmis" should "successfully add UMIs to a BAM" in {
     val out = makeTempFile("with_umis.", ".bam")
-    val annotator = new AnnotateBamWithUmis(input=sam, fastq=fq, output=out, attribute=umiTag)
+    val annotator = new AnnotateBamWithUmis(input=sam, fastq=Seq(fq), output=out, attribute=umiTag)
     annotator.execute()
     SamSource(out).foreach(rec => {
       rec[String](umiTag) shouldBe rec.basesString.substring(0,8)
     })
   }
 
-  "AnnotateBamWithUmis" should "fail if one or more reads doesn't have a UMI" in {
+  it should "fail if one or more reads doesn't have a UMI" in {
     val out     = makeTempFile("with_umis.", ".bam")
     val shortFq = makeTempFile("missing_umis.", ".fq.gz")
     Io.writeLines(shortFq, Io.readLines(fq).toSeq.dropRight(8))
-    val annotator = new AnnotateBamWithUmis(input=sam, fastq=shortFq, output=out, attribute=umiTag)
+    val annotator = new AnnotateBamWithUmis(input=sam, fastq=Seq(shortFq), output=out, attribute=umiTag)
     an[FailureException] shouldBe thrownBy { annotator.execute() }
   }
 
-  "AnnotateBamWithUmis" should "successfully add UMIs to a BAM with a given read structure in" in {
+  it should "successfully add UMIs to a BAM with a given read structure in" in {
     val out = makeTempFile("with_umis.", ".bam")
-    val annotator = new AnnotateBamWithUmis(input=sam, fastq=fq, output=out, attribute=umiTag, readStructure=ReadStructure("2B4M+B"))
+    val annotator = new AnnotateBamWithUmis(input=sam, fastq=Seq(fq), output=out, attribute=umiTag, readStructure=Seq(ReadStructure("2B4M+B")))
     annotator.execute()
     SamSource(out).foreach(rec => {
-      rec[String](umiTag) shouldBe rec.basesString.substring(2,6)
+      rec[String](umiTag) shouldBe rec.basesString.substring(2, 6)
     })
+  }
+
+  it should "successfully add UMIs to a BAM using all bases from multiple FASTQs" in {
+    val out = makeTempFile("with_umis.", ".bam")
+    val annotator = new AnnotateBamWithUmis(input=sam, fastq=Seq(fq, fq), readStructure=Seq(ReadStructure("2B+M")), output=out, attribute=umiTag, delimiter="x")
+    annotator.execute()
+    SamSource(out).foreach { rec =>
+      val bases = rec.basesString.substring(2,8)
+      rec[String](umiTag) shouldBe s"${bases}x${bases}"
+    }
+  }
+
+  it should "successfully add UMIs to a BAM with multiple FASTQs and corresponding read structures" in {
+    val out = makeTempFile("with_umis.", ".bam")
+    val annotator = new AnnotateBamWithUmis(input=sam, fastq=Seq(fq, fq), readStructure=Seq(ReadStructure("2M4B+M"), ReadStructure("1B+M")), output=out, attribute=umiTag, delimiter="x")
+    annotator.execute()
+    SamSource(out).foreach { rec =>
+      val first  = rec.basesString.substring(0,2) // read one: [2M]4B+M
+      val second = rec.basesString.substring(6,8) // read one: 2M4B[+M]
+      val third  = rec.basesString.substring(1,8) // read two: 1B[+M]
+      rec[String](umiTag) shouldBe s"${first}x${second}x${third}"
+    }
   }
 }
